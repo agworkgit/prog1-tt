15b039947eb084fb5755713b0a3353b90fc0d08e
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-07-12 17:48:46 +0100

    updated comments

--- 1_setup/ConsoleApp1/Application.cs
+++ 1_setup/ConsoleApp1/Application.cs
@@ -1,4 +1,9 @@
-﻿// External libraries part of the language to be included at compile time
+﻿// IDE - Integrated Development Environment (reads/writes code)
+// Compiler - Compiles the code down to executable machine code
+// Namespace - Acts as a container, groups similar classes, can be used to avoid naming conflicts in larger projects
+// Access Modifier - Defines the circumstances under which the method can be accessed: public, private, protected, internal
+
+// External libraries part of the language to be included at compile time
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +23,10 @@ namespace Comp100_Lesson3
     class Application
     {
         // This is called a main method (or function), here's our starting place for our code
+        // In this case, Main() is the method HEADER which defines what the method does
         static void Main()
         {
-            // Lines of code inside a codeblock
+            // Lines of code inside a codeblock, also called the method BODY
             int myInteger = 0;
             string myString = "I also wrote my first string";
             System.Console.WriteLine("The int variable value is " + myInteger + ", " + myString);
