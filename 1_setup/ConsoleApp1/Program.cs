9c9716f9a33092d5ae2ce54e40334fc154a5dbb0
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-07-12 16:22:09 +0100

    wrote first variable

--- 1_setup/ConsoleApp1/Program.cs
+++ 1_setup/ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello from C#!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp100_Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVariable = 0;
+            Console.WriteLine("The variable value is " + myVariable);
+        }
+    }
+}
