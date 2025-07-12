70520b302ae8f3e20caa8f1030a0284b398da9c0
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-07-12 16:42:54 +0100

    comments

--- 1_setup/ConsoleApp1/Program.cs
+++ 1_setup/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// External libraries part of the language to be included at compile time
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,10 @@ namespace Comp100_Lesson3
 {
     class Program
     {
-        static void Main(string[] args)
+        // This is called a method (or function)
+        static void Main()
         {
+            // Lines of code inside a codeblock
             int myInteger = 0;
             string myString = "I also wrote my first string";
             Console.WriteLine("The int variable value is " + myInteger + ", " + myString);
