22eb38a6502321fe78bf7f0ea2d8a5715da2436a
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-07-18 14:49:38 +0200

    learning operators

---
+++ 2_using_data/Using_Data/Program2.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP100_binary_operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* Using Arithmetic Operators
+            * + Addition
+            * - Subtraction
+            * * Multiplication
+            * / Division
+            * % Remainder (modulus/mod)
+            */
+
+            /* Using Shortcut Arithmetic Operators
+            - Add and assign operators
+            * Example : bankBal += bankBal * interestRate;
+            * Variations : -=, *=, /=, and %=
+            - Prefix increment operator
+            * Example : ++someValue;
+            - Suffix increment operator
+            * Example : someValue++;
+            */
+
+        }
+    }
+}
