f6f05a2ee04ed7f15d07c08854a28c63cdf88cfb
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-07-19 14:11:23 +0200

    practice code - conditionals

--- 4_conditionals/ConsoleConditionals/Program.cs
+++ 4_conditionals/ConsoleConditionals/Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 /* 
  - VALUE SUFFIXES REVIEW
@@ -30,12 +31,36 @@ namespace Conditionals
         {
             // Your code goes here from now until the end of the course
 
-            Console.Write("Insert a string to be character counted: ");
+            // Practice code - Daily allowance calc in Euros (conditionals)
 
-            string origString = Console.ReadLine();
-            int countThis = origString.Length;
-
-            Console.WriteLine("The string \"{0}\", has {1} characters", origString, countThis);
+            Console.WriteLine("Daily allowance calculator");
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("Do you have some money? (Y/N): ");
+            string init = Console.ReadLine();
+            if (init == "Y")
+            {
+                Console.Write("What is your current month balance?: ");
+                double prompt = Convert.ToDouble(Console.ReadLine());
+                if (prompt <= 100)
+                {
+                    Console.WriteLine("Your said you have {0}, that means you need to make a minimum of 30.00/per day to sustain yourself, come back when your income is higher than 100.00 to calculate your daily allowance.", prompt);
+                    Console.WriteLine("-----------------------------------------");
+                }
+                else
+                {
+                    double result = Convert.ToDouble(prompt) / 30;
+                    string formattedCurrencyItaly = string.Format(new CultureInfo("it-IT"), "{0:C}", result);
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine("Your daily allowance is {0}", formattedCurrencyItaly);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Go make some money first then come back.");
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
