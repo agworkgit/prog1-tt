1f56b00df0cb28a8d3c6063d4c99e62e7340ed02
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-07-20 11:46:04 +0200

    working with constants and precision

--- 5_constants/Constants/Program.cs
+++ 5_constants/Constants/Program.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ - Named constant
+ * Often simply called a constant
+ * An idetifier whose contents cannot change
+ * Created using the keyword const
+ 
+ - Programmers usually name constants using all uppercase letters in C#, inserting underscores for readability
+
+ - Self-documenting statement
+ * Easy to understand even without program comments
+ */
+
 namespace Constants
 {
     class Program
     {
         static void Main()
         {
-
+            const float PI = 3.14F; // PI is always the same
+            double circumference;
+            double radius;
+            Console.WriteLine("What is your radius?: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            circumference = 2 * PI * radius * radius;
+            Console.WriteLine("Your circle's circumference is {0}", Convert.ToSingle(circumference));
         }
     }
 }
