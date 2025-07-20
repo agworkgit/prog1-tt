a955eb1dc46fab76c391b186a3074d177afa6892
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-07-20 21:22:34 +0200

    learning flow charts, diagrams and decision tree

--- 5_constants/Constants/Program.cs
+++ 5_constants/Constants/Program.cs
@@ -16,19 +16,58 @@ using System.Threading.Tasks;
  * Easy to understand even without program comments
  */
 
+/* 
+ - Working with Enumerations
+ * An enumeration is a set of constants represented by identifiers
+ * Enumerations example named DayOfWeek:
+ * enum DayOfWeek
+ * {
+ *      SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+ * }
+ */
+
 namespace Constants
 {
     class Program
     {
+        enum DayOfWeek : int
+        {
+            SUNDAY = 1, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+        };
+
+        const double HST = 0.13d;
         static void Main()
         {
-            const float PI = 3.14F; // PI is always the same
+            const double PI = Math.PI; // PI is always the same
             double circumference;
             double radius;
             Console.WriteLine("What is your radius?: ");
             radius = Convert.ToDouble(Console.ReadLine());
             circumference = 2 * PI * radius * radius;
-            Console.WriteLine("Your circle's circumference is {0}", Convert.ToSingle(circumference));
+            Console.WriteLine("Your circle's circumference is {0}", Math.Round(circumference));
+
+            Console.WriteLine((int)DayOfWeek.SUNDAY); // cast as int
+
+            // Conditional exercise
+
+            Console.Write("What's your age?: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age <= 10)
+            {
+                Console.WriteLine("You're too young!");
+            }
+            else if (age <= 30)
+            {
+                Console.WriteLine("The world is your oyster!");
+            }
+            else
+            {
+                Console.WriteLine("You're getting old!");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
---
+++ 5_constants/objectives.md
@@ -0,0 +1,31 @@
+# Objectives
+
+1. Understand logic-planning tools and decision making
+2. Learn how to make decisions using the if statement
+3. Learn how to make decisions using the if/else statement
+4. Use compound expressions in if statements
+5. Make decisions using the switch statement
+6. Use the conditional operator
+7. Use the NOT notation
+8. Learn to avoid common errors when making decisions
+9. Learn about decision-making issues in GUI programs
+
+## Understanding Logic-Planning Tools and Decision Making
+
+-   Pseudocode
+    A tool that helps programmers plan a program's logic by writing plain English statements
+-   Flowchart
+    You write steps in diagram form as a series of shapes connected by arrows
+-   Sequence structure
+    One step follows another unconditionally and sequentially
+    Sometimes, logical steps do not follow in an unconditional sequence
+-   Decision structure
+    Involves choosing between alternative courses of action based on some value within a program
+    All computer decisions are yes-or-no decisions when reduced to their most basic form
+
+## Making Decisions Using if Statement
+
+-   if statement
+    Used to make a single-alternative decision
+-   Block
+    One or more statements contained within a pair of curly braces
